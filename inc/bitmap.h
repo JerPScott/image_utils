1ad2251ec54ecf401cb5f2a1a6bb624984f724b7
Jeremy Scott <Jer.P.Scott@gmail.com>
2018-09-23 00:16:37 -0700

    Add ability to write to section of image_pixels

--- inc/bitmap.h
+++ inc/bitmap.h
@@ -5,6 +5,8 @@
 #include <memory>
 #include <array>
 
+#include <cassert>
+
 #include "debug.h"
 
 namespace detail
@@ -43,7 +45,10 @@ struct image_bytes
   using ptr = std::shared_ptr<image_bytes<w, h>>;
   using const_ptr = std::shared_ptr<const image_bytes<w, h>>;
 
-  inline pixel const& get(unsigned int x, unsigned int y)
+  static const unsigned int width = w;
+  static const unsigned int height = w;
+
+  inline pixel const& get(unsigned int x, unsigned int y) const
   {
     return m_data[y][x];
   }
@@ -53,15 +58,47 @@ struct image_bytes
     return m_data[y][x];
   }
 
-  inline unsigned char get_r(unsigned int x, unsigned int y)
+  void set(pixel const& fill)
+  {
+    for (auto& row : m_data)
+    {
+      for (auto& pix : row)
+      {
+        pix = fill;
+      }
+    }
+  }
+
+  template<unsigned int image_width, unsigned int image_height>
+  void set(
+    unsigned int x_start,
+    unsigned int y_start,
+    image_bytes<image_width, image_height> const& image)
+  {
+    assert((x_start + image_width) <= width);
+    assert((y_start + image_height) <= height);
+    
+    for (unsigned int x = 0; x < image_width; ++x)
+    {
+      for (unsigned int y = 0; y < image_width; ++y)
+      {
+        auto curr_x = x + x_start;
+        auto curr_y = y + y_start;
+
+        at(curr_x, curr_y) = image.get(x, y);
+      }
+    }
+  }
+
+  inline unsigned char get_r(unsigned int x, unsigned int y) const
   {
     return m_data[y][x][0];
   }
-  inline unsigned char get_g(unsigned int x, unsigned int y)
+  inline unsigned char get_g(unsigned int x, unsigned int y) const
   {
     return m_data[y][x][1];
   }
-  inline unsigned char get_b(unsigned int x, unsigned int y)
+  inline unsigned char get_b(unsigned int x, unsigned int y) const
   {
     return m_data[y][x][2];
   }
--- src/bmp.cpp
+++ src/bmp.cpp
@@ -4,30 +4,33 @@ int main()
 {
   const unsigned int k_width = 1040;
   const unsigned int k_height = 700;
-  const pixel k_blank_pixel = { 0, 0, 0 };
+  //const pixel k_black_pixel = { 0, 0, 0 };
+  const pixel k_white_pixel = { 255, 255, 255 };
   const pixel k_r_pixel = { 255, 0, 0 };
   const pixel k_g_pixel = { 0, 255, 0 };
   const pixel k_b_pixel = { 0, 0, 255 };
 
   auto test_image = std::make_shared<image_bytes<k_width, k_height>>();
 
+  auto active_color = k_r_pixel;
+  auto passive_color = k_g_pixel;
   for (unsigned int x = 0; x < k_width; x++)
   {
-    for (unsigned int y = 0; y < k_height/4; y++)
+    for (unsigned int y = 0; y < k_height; y++)
     {
-      test_image->at(x, y) = k_r_pixel;
+      if ((x%50<25) == (y%50<25))
+      {
+        test_image->at(x, y) = active_color;
+      }
+      else
+      {
+        test_image->at(x, y) = passive_color;
+      }
     }
-    for (unsigned int y = k_height/4; y < k_height*2/4; y++)
+    if (x == k_width/2)
     {
-      test_image->at(x, y) = k_g_pixel;
-    }
-    for (unsigned int y = k_height*2/4; y < k_height*3/4; y++)
-    {
-      test_image->at(x, y) = k_b_pixel;
-    }
-    for (unsigned int y = k_height*3/4; y < k_height; y++)
-    {
-      test_image->at(x, y) = k_blank_pixel;
+      active_color = k_white_pixel;
+      passive_color = k_b_pixel;
     }
   }
 
